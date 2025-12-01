431e95fcf682033acc2aa990f593607d87623ed1
devil <europedreadlydevil@gmail.com>
2025-12-01 20:06:36 +0300

    Implement C ABI for SimplePlugin
    This change implements the C ABI functions required for the plugin to be loaded and used by the Lakehouse CLI. It also updates the `plugin` function to return a pointer to a `PluginApi` struct.

--- examples/simple_plugin_example/src/lib.rs
+++ examples/simple_plugin_example/src/lib.rs
@@ -1,5 +1,7 @@
-use clap::{Command, arg};
-use lake_house_cli_api::api::Plugin;
+use std::ffi::{c_char, c_void};
+
+use clap::{ArgMatches, Command, arg};
+use lake_house_cli_api::api::{Plugin, PluginApi};
 
 pub struct SimplePlugin;
 
@@ -25,7 +27,42 @@ impl Plugin for SimplePlugin {
     }
 }
 
+impl SimplePlugin {
+    extern "C" fn instantiate() -> *mut c_void {
+        Box::into_raw(Box::new(SimplePlugin)) as *mut c_void
+    }
+
+    extern "C" fn name(ptr: *mut c_void) -> *const c_char {
+        let this = unsafe { &*(ptr as *mut SimplePlugin) };
+        std::ffi::CString::new(this.name()).unwrap().into_raw()
+    }
+
+    extern "C" fn command(ptr: *mut c_void) -> *mut Command {
+        let this = unsafe { &*(ptr as *mut SimplePlugin) };
+        Box::into_raw(Box::new(this.command()))
+    }
+
+    extern "C" fn run(ptr: *mut c_void, matches: *const ArgMatches) -> i32 {
+        let this = unsafe { &*(ptr as *mut SimplePlugin) };
+        if this.run(unsafe { (*matches).clone() }).is_ok() {
+            0
+        } else {
+            1
+        }
+    }
+
+    extern "C" fn drop(ptr: *mut c_void) {
+        unsafe { drop(Box::from_raw(ptr as *mut SimplePlugin)) };
+    }
+}
+
 #[unsafe(no_mangle)]
-extern "C" fn plugin() -> *mut SimplePlugin {
-    Box::leak(Box::new(SimplePlugin)) as *mut _
+pub extern "C" fn plugin() -> *const PluginApi {
+    &PluginApi {
+        instantiate: SimplePlugin::instantiate,
+        name: SimplePlugin::name,
+        command: SimplePlugin::command,
+        run: SimplePlugin::run,
+        drop: SimplePlugin::drop,
+    }
 }
